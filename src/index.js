831ebc29f1b964c17bbdf8c5fb32580d2a569eb9
SeriAce <SeriAce@hotmail.de>
2022-09-06 09:04:43 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "./divs.css";
@@ -13,7 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/*" element={<App />}>
+          <Route index element={<App />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
